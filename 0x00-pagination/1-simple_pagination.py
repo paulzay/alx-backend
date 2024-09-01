73dfdadafc0e205c7e943b516e94e0cd9bed87d5
Paul Ogolla <paulotieno2@gmail.com>
2024-09-01 19:10:37 +0300

    1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""simple pagination"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """return tuple of indices"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    data_tuple = tuple(start_index, end_index)
+    return data_tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page from dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
