09eebc335e459418a433e6062608c0b16f38df00
Paul Ogolla <paulotieno2@gmail.com>
2024-09-01 19:04:54 +0300

    0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """simple helper function"""
+from typing import Tuple
 
 
-def index_range(page, page_size):
+
+def index_range(page, page_size) -> Tuple[int, int]:
     """return tuple of indices"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
