cfb692fd2ec91cc7e2f1e36675b056a3ba0291dd
Paul Ogolla <paulotieno2@gmail.com>
2024-09-02 12:46:32 +0300

    0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,4 @@ def index_range(page: int, page_size: int) -> tuple:
     """return tuple of indices"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    data_tuple = tuple(start_index, end_index)
-    return data_tuple
+    return (start_index, end_index)
