a7baf1b7ea9d792e14bbb9ec4f07257706002608
Paul Ogolla <paulotieno2@gmail.com>
2024-09-04 22:07:40 +0300

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Basic Cache Module """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ child class """
+    def put(self, key, item):
+        """ put cache """
+        if item and key:
+            self.cache_data = key
+
+    def get(self, key):
+        """ get cache """
+        return self.cache_data.get(key)
