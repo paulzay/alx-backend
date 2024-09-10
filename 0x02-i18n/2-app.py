6ab1cd33a0dff74730a2f033934639e62490b662
Paul Ogolla <paulotieno2@gmail.com>
2024-09-10 21:42:06 +0300

    2 get locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" basic flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+def get_locale():
+    """ get locale """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ display h1 and p"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
