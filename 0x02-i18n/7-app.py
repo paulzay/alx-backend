583664e849c1fe216b22114763ecf567d6df68b0
Paul Ogolla <paulotieno2@gmail.com>
2024-09-10 22:35:38 +0300

    updates

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -2,6 +2,7 @@
 """ basic flask app """
 from flask import Flask, render_template, request
 from flask_babel import Babel
+import pytz
 
 app = Flask(__name__)
 
@@ -49,6 +50,31 @@ def before_request() -> None:
     user = get_user(request.args.get('login_as'))
     if user:
         g.user = user
+
+@babel.localeselector
+def get_locale() -> str:
+    """ get locales """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """Retrieves the timezone """
+    timezone = request.args.get('timezone', '').strip()
+    if not timezone and g.user:
+        timezone = g.user['timezone']
+    try:
+        return pytz.timezone(timezone).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        return app.config['BABEL_DEFAULT_TIMEZONE']
 
 
 @app.route('/', strict_slashes=False)
