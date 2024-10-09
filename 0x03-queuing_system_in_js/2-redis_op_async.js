6f27d9dce272bebeb93b73460bceadfed40bedb5
Paul Ogolla <paulotieno2@gmail.com>
2024-10-09 07:13:14 +0300

    task 2

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import { createClient } from 'redis';
+const util = require('util');
+
+const getAsync = util.promisify(client.get).bind(client);
+getAsync('key').then(console.log);
+
+const client = await createClient({
+  host: 'localhost',
+  port: 6379,
+  functions: {
+    setNewSchool: () => {
+      console.log('SETNEW')
+    },
+    displaySchoolValue: () => {
+      console.log('DISPLAY')
+    },
+  }
+})
+  .on('connect', () => {
+    console.log('Redis client connected to the server ')
+  })
+  .on('error', err => {
+    console.log('Redis client not connected to the server:', err)
+  })
+  .connect();
+
+client.setNewSchool('Holberton');
+client.setNewSchool('HolbertonSanFrancisco', '100');
+client.displaySchoolValue('HolbertonSanFrancisco');
+
+await client.set('key', 'value');
+const value = await client.get('key');
+await client.disconnect();
